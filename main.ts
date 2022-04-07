346d16bd9dc02b03a5ac4dde8d21fd8f3978e244
jordicollell <78138546+jordicollell@users.noreply.github.com>
2022-04-07 09:09:59 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,52 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("ESQUERRA")
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("PARAR")
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendString("DRETA")
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("ENRRERA")
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendString("ENDAVANT")
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
 })
 radio.setGroup(1)
 basic.showLeds(`
